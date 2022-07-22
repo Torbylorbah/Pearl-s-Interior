6a385a10d48b2062545e7bdd2c1b838f1df796d1
Owodunni Jalalatullahi <jaybukky7@gmail.com>
2022-07-22 20:03:20 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,5 +22,20 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+      'tailwindcss',
+      'postcss-flexbugs-fixes',
+      [
+        'postcss-preset-env',
+        {
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+          features: {
+            'custom-properties': false,
+          },
+        },
+      ],
+    ],    
 }
